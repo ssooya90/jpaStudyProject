87d2699bd845cbc1f8b83d1e0257ee67dd8ea881
ssooya90 <ssooya90@naver.com>
2020-02-01 10:12:47 +0900

    실전연습 Member객체 생성

---
+++ model1/src/main/java/ssooya/model/entity/Member.java
@@ -0,0 +1,20 @@
+package ssooya.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Member {
+
+	@Id @GeneratedValue
+	@Column(name ="MEMBER_ID")
+	private Long id;
+
+	private String name;
+
+	private String city;
+	private String street;
+	private String zipcode;
+}
